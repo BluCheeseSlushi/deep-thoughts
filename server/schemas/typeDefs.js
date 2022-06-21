ba6d8eae72ae0459916ab5a7aad492c6b3129d54
Josiah Douglass <josiahdouglass64@gmail.com>
2022-06-21 17:14:50 -0400

    apollo set up

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,22 @@
 const { gql } = require('apollo-server-express');
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+    }
+
+    type Query {
+        thoughts {
+            _id
+            username
+            thoughtText
+            createdAt
+        }
+    }
+`;
 
 
 module.exports = typeDefs;
